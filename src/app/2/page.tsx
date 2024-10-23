748a5a528049fe6d6e694e9810e849e65b27cd69
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-10-23 14:27:34 +0530

    option 2 complete

--- src/app/2/page.tsx
+++ src/app/2/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
-// import { FlipWords } from "../components/ui/flip-words";
+import { FlipWords } from "../components/ui/flip-words";
+import Footer from "../components/ui/Footer";
 
 export default function Home() {
   const links = ["Home", "Manifesto", "Services", "Connect"];
@@ -52,32 +53,39 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col w-full h-screen bg-black bg-grid-white/[0.2]">
-      <div className="flex mt-16 pt-20 h-fit mx-auto">
-            <p className="text-6xl font-black text-white">
-              THE IDEOLOGY
-            </p>
-      </div>
-        <div className="flex flex-col m-auto ">
-          {messages.map((message, index) => {
-            return (
-              <>
-                <p className="flex text-4xl font-bold text-white" key={index}>
-                  {/* <FlipWords/> */}
-                  {message.message}
-                </p>{" "}
-                <br />
-                <p
-                  className="flex ml-auto italic text-3xl font-bold text-white mb-10"
-                  key={index}
-                >
-                  {"-" + message.who}
-                </p>
-              </>
-            );
-          })}
+      <div className="flex flex-col w-full h-[100vh] bg-black bg-grid-white/[0.2]">
+        <div className="flex mt-16 pt-20 h-fit mx-auto ">
+          <p className="text-6xl font-black text-black bg-yellow-400 px-4 py-2">
+            THE IDEOLOGY
+          </p>
         </div>
+        <div className="flex flex-col w-1/3 m-auto">
+          <span className="flex text-4xl font-black text-white w-fit rounded italic mb-5 ml-4">
+            <FlipWords
+              words={messages.map((item) => item.who)}
+              duration={5000}
+            />{" "}
+            :
+          </span>
+          <span className="flex text-4xl font-bold text-white w-fit rounded text-center">
+            <FlipWords
+              words={messages.map((item) => item.message)}
+              duration={5000}
+            />
+          </span>
+          <br />
+        </div>
+
+        <p className="text-neutral-300 text-base font-medium text-center mx-auto pb-20">
+          Ardent Co. will always be a sandbox for inspired, driven
+          professionals. <br />
+          Ardent Co. will never limit itself by any notions and perceptions.{" "}
+          <br />
+          Ardent Co. will encourage both thinkers and doers, and serve as an
+          ecosystem for unleashing untapped potential.
+        </p>
       </div>
+      <Footer />
     </>
   );
 }
