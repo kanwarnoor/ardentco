80a239bf6d4c629588ec6c9b4c1a70f53e301409
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-10-30 23:52:29 +0530

    some update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-"use client"
+"use client";
+
+import { motion } from "framer-motion";
+import { FlipWords } from "./components/ui/flip-words";
+import Link from "next/link";
 
 export default function Home() {
+  const avail: string[] = [
+    "Rewiring",
+    "Transforming",
+    "Rewriting",
+    "Modernizing",
+    "Elevating",
+  ];
   return (
-    <div className="w-full h-fit absolute top-0 bottom-0 left-0 right-0 m-auto text-center flex flex-col justify-center items-center">
-      <p className="font-bold mb-5 sm:text-5xl text-3xl">new page</p> 
-    </div>
+    <>
+      <div className="w-full h-fit absolute top-0 bottom-0 left-0 right-0 m-auto text-center flex flex-col justify-center items-center bg-black text-white">
+        <div>
+          <FlipWords
+            words={avail}
+            duration={3000}
+            className="text-9xl font-bold text-center"
+          />
+
+          <motion.p
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 0.6,
+            }}
+            transition={{
+              duration: 1,
+            }}
+            className="text-7xl font-bold"
+          >
+            Status Quo
+          </motion.p> <br />
+
+            
+          <Link href="" className="text-2xl border-2 font-bold border-white px-5 py-2 rounded-full">Book a call</Link>
+        </div>
+      </div>
+    </>
   );
 }
