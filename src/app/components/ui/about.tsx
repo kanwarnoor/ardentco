f4693f12fbe5fa9b3a77f24c0806d9d6795d51c8
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-11-03 14:07:35 +0530

    minor changes

--- src/app/components/ui/about.tsx
+++ src/app/components/ui/about.tsx
@@ -27,98 +27,118 @@ export default function About() {
   return (
     <div className="flex h-screen z-10">
       <div className="w-fit h-fit flex m-auto">
-        <motion.div
-          initial={{
-            opacity: 0,
-            x: "-10%",
-            rotateY: 0,
-          }}
-          animate={{
-            rotateY: clicked ? 180 : 0,
-            transition: {
-              duration: 0.5,
-            },
-          }}
-          whileInView={{
-            opacity: 1,
-            x: 0,
-            transition: {
-              duration: 0.5,
-            },
-          }}
-          viewport={{
-            amount: 0.3,
-          }}
-          whileHover={{
-            scale: 1.02,
-            transition: {
-              duration: 0.1,
-            },
-          }}
-          transition={{
-            duration: 0.1,
-          }}
-          onClick={() => setClicked(!clicked)}
-          className={`flex flex-col mr-10 w-[400px] h-[400px]  cursor-pointer rounded-2xl bg-black`}
-        >
-          {visible && (
-            <motion.div
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: 1,
-                transition: {
-                  duration: 0.5,
-                },
-              }}
-              exit={{
-                opacity: 0,
-                transition: {
-                  duration: 0.5,
-                },
-              }}
-              transition={{
+        <div className="z-10 w-fit  mr-10">
+          <motion.div
+            initial={{
+              opacity: 0,
+              x: "-10%",
+              rotateY: 0,
+            }}
+            animate={{
+              rotateY: clicked ? 180 : 0,
+              transition: {
                 duration: 0.5,
-              }}
-              viewport={{
-                amount: 0.3,
-              }}
-              style={{
-                rotateY: -180,
-              }}
-            >
-              <p
-                className={`text-white px-10 pt-10 pb-5 text-center text-2xl font-bold`}
+              },
+            }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+              transition: {
+                duration: 0.5,
+              },
+            }}
+            viewport={{
+              amount: 0.3,
+            }}
+            whileHover={{
+              scale: 1.02,
+              transition: {
+                duration: 0.1,
+              },
+            }}
+            transition={{
+              duration: 0.1,
+            }}
+            onClick={() => setClicked(!clicked)}
+            className={`flex flex-col w-[400px] h-[400px] cursor-pointer rounded-2xl bg-black`}
+          >
+            {visible && (
+              <motion.div
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                  transition: {
+                    duration: 0.5,
+                  },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: {
+                    duration: 0.5,
+                  },
+                }}
+                transition={{
+                  duration: 0.5,
+                }}
+                viewport={{
+                  amount: 0.3,
+                }}
+                style={{
+                  rotateY: -180,
+                }}
               >
-                Nalanda Mahavihara
-              </p>
-              <p className="text-white px-10 text-center text-sm ">
-                Ardent Co. embodies the spirit of Nalanda University by serving
-                as a modern center for knowledge and impactful dialogue. Just as
-                Nalanda attracted scholars from around the world to exchange
-                ideas and foster learning, Ardent Co. brings together diverse
-                insights and expertise to craft innovative, purpose-driven
-                strategies. Our commitment to bridging ambition and action
-                mirrors {"Nalanda’s"} dedication to intellectual advancement,
-                ensuring that we empower our clients to navigate complex
-                landscapes and create meaningful impact in society.
-              </p>
-            </motion.div>
-          )}
+                <p
+                  className={`text-white px-10 pt-10 pb-5 text-center text-2xl font-bold `}
+                >
+                  Nalanda Mahavihara
+                </p>
+                <p className="text-white px-10 text-center text-sm">
+                  Ardent Co. embodies the spirit of Nalanda University by
+                  serving as a modern center for knowledge and impactful
+                  dialogue. Just as Nalanda attracted scholars from around the
+                  world to exchange ideas and foster learning, Ardent Co. brings
+                  together diverse insights and expertise to craft innovative,
+                  purpose-driven strategies. Our commitment to bridging ambition
+                  and action mirrors {"Nalanda’s"} dedication to intellectual
+                  advancement, ensuring that we empower our clients to navigate
+                  complex landscapes and create meaningful impact in society.
+                </p>
+              </motion.div>
+            )}
 
-          <Image
-            src="/places/Nalanda.jpg"
-            height={0}
-            width={0}
-            sizes="100%, 100%"
-            alt="compass"
-            className={`flex w-[400px] h-[400px] bg-red-200 transition rounded-2xl cursor-pointer duration-[350ms] z-10 ${
-              clicked ? "opacity-0" : "opacity-100"
-            }`}
-          ></Image>
-        </motion.div>
-
+            <Image
+              src="/places/Nalanda.jpg"
+              height={0}
+              width={0}
+              sizes="100%, 100%"
+              alt="compass"
+              className={`flex w-[400px] h-[400px]  transition rounded-2xl cursor-pointer duration-[350ms] z-10 ${
+                clicked ? "opacity-0" : "opacity-100"
+              }`}
+            ></Image>
+          </motion.div>
+          <motion.p
+            initial={{
+              opacity: 0,
+              x: "-10%",
+            }}
+            whileInView={{
+              opacity: 1,
+              x: "0%",
+              transition: {
+                duration: 0.5,
+              },
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="z-20 text-center text-xs mt-2"
+          >
+            Ruins of Nalanda University - Bihar
+          </motion.p>
+        </div>
         <div className="w-[700px] h-fit flex flex-col justify-center my-auto ">
           <motion.p
             initial={{
