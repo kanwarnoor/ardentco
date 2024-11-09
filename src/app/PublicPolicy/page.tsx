c0906010309fd67846bd9a76f8e40834824b2500
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-11-09 15:53:17 +0530

    about page

---
+++ src/app/PublicPolicy/page.tsx
@@ -0,0 +1,311 @@
+"use client";
+
+import React from "react";
+import Navbar from "../components/ui/Navbar";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Footer from "../components/ui/Footer";
+
+export default function page() {
+  return (
+    <>
+      <Navbar />
+      <section
+        id="home"
+        className="relative w-full h-screen m-auto text-center flex flex-col justify-center items-center bg-opacity-100 bg-white"
+      >
+        <motion.div
+          initial={{
+            scale: 0.8,
+            borderRadius: "1rem",
+          }}
+          animate={{
+            scale: 1,
+            borderRadius: "0px",
+          }}
+          transition={{
+            duration: 0.3,
+          }}
+          className="relative object-cover inset-0 w-full h-full scale-90 rounded-2xl opacity-100"
+        >
+          <Image
+            src="/UnitedNations.jpg"
+            height={0}
+            width={0}
+            sizes="100% 100%"
+            className="absolute object-cover inset-0 w-full h-full scale-100 opacity-100"
+            alt="athens"
+            no-select="true"
+          ></Image>
+        </motion.div>
+        <motion.div
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 0.7,
+          }}
+          transition={{
+            duration: 2,
+          }}
+          className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(circle,white_10%,transparent_50%)]"
+        ></motion.div>
+        <div className="absolute">
+          <motion.span
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            transition={{
+              duration: 1,
+            }}
+            className="text-7xl font-black text-black bg-yellow-400 leading-tight"
+          >
+            Public policy & <br /> Regulatory Affairs
+          </motion.span>
+          <br />
+        </div>
+      </section>
+
+      <section
+        className="relative bg-white bg-dot-black/[0.5] w-full h-auto min-h-screen"
+        id="about"
+      >
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(circle,white_10%,transparent_100%)] z-0"></div>
+
+        <div className="relative m-auto flex flex-col w-full h-auto py-20 z-10 justify-center items-center min-h-screen gap-5">
+          <div className="flex flex-wrap justify-center w-full h-fit text-center gap-5 mx-auto">
+            <motion.div
+              initial={{
+                opacity: 0,
+                x: -20,
+              }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+              className="flex flex-col w-[400px] h-[400px] border-2 border-black border-dashed rounded-2xl items-center"
+            >
+              <div className="flex flex-col h-fit m-auto justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="black"
+                  className="size-10"
+                >
+                  <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+
+                <p className="text-black text-4xl font-bold m-auto">
+                  Brand Image Development
+                </p>
+                <p className="text-black text-sm px-10 mt-5">
+                  Ardent Co. leverages its robust methodologies and
+                  communications expertise in defining and executing ground-up
+                  image-development strategies for organizations cutting across
+                  scales and sectors. We deploy a systematic approach in
+                  acquiring a robust understanding of the core-business and
+                  organizational journey, and translating those into strategies
+                  which deliver balanced conversations on both, the heritage and
+                  the future
+                </p>
+              </div>
+            </motion.div>
+
+            <motion.div
+              initial={{
+                opacity: 0,
+                scale: 0.9,
+              }}
+              whileInView={{
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+              className="flex flex-col w-[400px] h-[400px] border-2 border-black border-dashed rounded-2xl  items-center"
+            >
+              <div className="flex flex-col h-fit  m-auto justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-10"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941"
+                  />
+                </svg>
+
+                <p className="text-black text-4xl font-bold m-auto">
+                  Corporate Communications
+                </p>
+                <p className="text-black text-sm px-10 mt-5">
+                  Ardent Co is appreciated for bringing fresh-perspectives to
+                  the Corporate Affairs function, enabling leaders in leveraging
+                  communications as a business-aligned tool, while sustaining
+                  its effort in ensuring awareness and consistency in
+                  organizational media communications. Our cross-functional team
+                  enables organizations in reimagining the horizons of Corporate
+                  Communications and how it can align more closely with the
+                  core-business.
+                </p>
+              </div>
+            </motion.div>
+
+            <motion.div
+              initial={{
+                opacity: 0,
+                x: 20,
+              }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+              className="flex flex-col w-[400px] h-[400px] border-2 border-black border-dashed rounded-2xl  items-center"
+            >
+              <div className="flex flex-col h-fit  m-auto justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-10"
+                >
+                  <path d="M6 12a.75.75 0 0 1-.75-.75v-7.5a.75.75 0 1 1 1.5 0v7.5A.75.75 0 0 1 6 12ZM18 12a.75.75 0 0 1-.75-.75v-7.5a.75.75 0 0 1 1.5 0v7.5A.75.75 0 0 1 18 12ZM6.75 20.25v-1.5a.75.75 0 0 0-1.5 0v1.5a.75.75 0 0 0 1.5 0ZM18.75 18.75v1.5a.75.75 0 0 1-1.5 0v-1.5a.75.75 0 0 1 1.5 0ZM12.75 5.25v-1.5a.75.75 0 0 0-1.5 0v1.5a.75.75 0 0 0 1.5 0ZM12 21a.75.75 0 0 1-.75-.75v-7.5a.75.75 0 0 1 1.5 0v7.5A.75.75 0 0 1 12 21ZM3.75 15a2.25 2.25 0 1 0 4.5 0 2.25 2.25 0 0 0-4.5 0ZM12 11.25a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5ZM15.75 15a2.25 2.25 0 1 0 4.5 0 2.25 2.25 0 0 0-4.5 0Z" />
+                </svg>
+
+                <p className="text-black text-4xl font-bold m-auto">
+                  Startup <br /> Advisory
+                </p>
+                <p className="text-black text-sm px-10 mt-5">
+                  At Ardent, we design and execute strategies that strike a
+                  balance between the founders’ vision, business fundamentals,
+                  and product nuances. We deeply understand the passion and
+                  persistence of entrepreneurs, delivering tailored solutions
+                  with relentless determination. Our approach combines strategic
+                  insight with a keen eye on the unique demands of each business
+                  journey, driving impactful outcomes that align legacy and
+                  growth aspirations.
+                </p>
+              </div>
+            </motion.div>
+          </div>
+
+          <div className="flex flex-wrap justify-center w-full h-fit text-center gap-5 mx-auto">
+            <motion.div
+              initial={{
+                opacity: 0,
+                x: -20,
+              }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+              className="flex flex-col w-[400px] h-[400px] border-2 border-black border-dashed rounded-2xl items-center"
+            >
+              <div className="flex flex-col h-fit m-auto justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-10"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.5 5.25a3 3 0 0 1 3-3h3a3 3 0 0 1 3 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0 1 12 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 0 1 7.5 5.455V5.25Zm7.5 0v.09a49.488 49.488 0 0 0-6 0v-.09a1.5 1.5 0 0 1 1.5-1.5h3a1.5 1.5 0 0 1 1.5 1.5Zm-3 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+                    clipRule="evenodd"
+                  />
+                  <path d="M3 18.4v-2.796a4.3 4.3 0 0 0 .713.31A26.226 26.226 0 0 0 12 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 0 1-6.477-.427C4.047 21.128 3 19.852 3 18.4Z" />
+                </svg>
+
+                <p className="text-black text-4xl font-bold m-auto">
+                  Comms Training & <br />
+                  Workshops
+                </p>
+                <p className="text-black text-sm px-10 mt-5">
+                  At Ardent Co, we understand the fast-paced business
+                  environment and the importance of effective communication with
+                  the media for senior executives. Our tailored Communications
+                  Training and Workshops equip leaders with essential skills to
+                  engage with journalists and handle media interactions.
+                  Combining academic insights and practical exercises, we help
+                  leaders understand the nuances of media engagement while
+                  aligning messages on key issues.
+                </p>
+              </div>
+            </motion.div>
+
+            <motion.div
+              initial={{
+                opacity: 0,
+                x: 20,
+              }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+              className="flex flex-col w-[400px] h-[400px] border-2 border-black border-dashed rounded-2xl  items-center"
+            >
+              <div className="flex flex-col h-fit  m-auto justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-10"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941"
+                  />
+                </svg>
+
+                <p className="text-black text-4xl font-bold m-auto">
+                  Leadership Comms Advisory
+                </p>
+                <p className="text-black text-sm px-10 mt-5">
+                  Ardent Co’s Leadership Communications Advisory helps
+                  organizations foster impactful and meaningful conversations
+                  led by their executives. We specialize in defining strategic
+                  topics for leaders to engage with, both within and outside
+                  their sector, while guiding them in driving discussions
+                  through media and public engagements. Our approach ensures
+                  leaders shape key narratives that align with their
+                  organization&apos;s goals.
+                </p>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </section>
+
+      <Footer theme={"dark"} />
+    </>
+  );
+}
