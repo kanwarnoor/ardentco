11a88827240f39b56fe69fabbb7249a44185e176
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-11-10 14:44:26 +0530

    all changes

--- src/app/components/ui/Wake.tsx
+++ src/app/components/ui/Wake.tsx
@@ -32,16 +32,12 @@ export default function Wake() {
       events.forEach((event) => window.removeEventListener(event, resetTimer));
       clearTimeout(timer);
     };
-  }, []);
+  });
 
   return (
-    // active && (
-    //   <>
-        
-    //   </>
-    // )
-
-    <motion.div
+    active && (
+      <>
+        <motion.div
           initial={{
             opacity: 0,
           }}
@@ -55,9 +51,11 @@ export default function Wake() {
         >
           <div className="text-center text-white flex m-auto flex-col">
             <p className="text-4xl font-black">
-              You can't sleep on us like that!
+              You can&apos;t sleep on us like that!
             </p>
           </div>
         </motion.div>
+      </>
+    )
   );
 }
