8bd230d48c3e0209009024b77bb744e87dcf3097
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-12-08 20:38:53 +0530

    animation

--- src/app/manifesto/page.tsx
+++ src/app/manifesto/page.tsx
@@ -43,11 +43,37 @@ export default function Home() {
         className="flex flex-col w-full min-h-screen h-fit bg-black bg-grid-white/[0.2] text-center"
       >
         <div className="flex flex-col w-fit h-fit m-auto md:pt-32 md:pb-32 pt-16 pb-16 text-center justify-center items-center">
-          <p className="text-3xl w-fit md:text-6xl m-auto font-black text-black bg-ardentLighter px-4 py-2">
+          <motion.p
+            initial={{
+              opacity: 0,
+              y: -50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="text-3xl w-fit md:text-6xl m-auto font-black text-black bg-ardentLighter px-4 py-2"
+          >
             THE IDEOLOGY
-          </p>
+          </motion.p>
 
-          <p className=" text-neutral-300  text-xs md:text-base font-medium w-[70%] mt-10">
+          <motion.p
+            initial={{
+              opacity: 0,
+              y: -25,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className=" text-neutral-300  text-xs md:text-base font-medium w-[70%] mt-10"
+          >
             Ardent Co, in its shape today is a company. In its soul, it’s a
             vision. In its intentions it’s journeying to become an
             institution.In our experience of working in the fields of
@@ -62,37 +88,51 @@ export default function Home() {
             founders, to stay honest to the vision of Ardent Co, we have written
             this manifesto to engrave our commitments to our stakeholders, of
             what Ardent Co will always be.
-          </p>
+          </motion.p>
 
-            <p className="flex text-3xl md:text-5xl w-fit text-white font-black mt-20 bg-ardent px-4 py-2">
-              OUR MANIFESTO <br /> OF COMMITMENTS
-            </p>
+          <motion.p
+            initial={{
+              opacity: 0,
+              y: 25,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="flex text-3xl md:text-5xl w-fit text-white font-black mt-20 bg-ardent px-4 py-2"
+          >
+            OUR MANIFESTO <br /> OF COMMITMENTS
+          </motion.p>
           <motion.div
             initial={{
               opacity: 0,
+              y: 50,
             }}
-            animate={{
+            whileInView={{
               opacity: 1,
+              y: 0,
             }}
             transition={{
-              delay: 1,
-              duration: 1,
+              duration: 0.5,
             }}
-            className="w-[70%] md:w-1/3 h-[200px] max-w-full mt-10"
+            className=" w-[70%] md:w-1/3 h-[200px] max-w-full mt-10"
           >
-            <span className="flex text-2xl md:text-4xl font-black text-white rounded italic mb-5">
+            <div className="flex text-2xl md:text-4xl font-black text-white rounded italic mb-5">
               <FlipWords
                 words={messages.map((item) => item.who)}
                 duration={5000}
               />{" "}
               :
-            </span>
-            <span className="flex text-2xl md:text-4xl font-bold text-white rounded text-center justify-center">
+            </div>
+            <div className="flex text-2xl md:text-4xl font-bold text-white rounded text-center justify-center">
               <FlipWords
                 words={messages.map((item) => item.message)}
                 duration={5000}
               />
-            </span>
+            </div>
           </motion.div>
           {/* <Image src={"/signature.png"} height={100} width={100} alt={"signature"} ></Image> */}
         </div>
