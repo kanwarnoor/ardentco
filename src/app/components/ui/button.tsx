ce10d36d4e5e64b8259318a24bee7a3301501a01
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-12-24 14:51:51 +0530

    perspective page

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -3,19 +3,23 @@ import React from "react";
 interface buttonProps {
   buttonTxt?: string;
   link: string;
+  theme?: "dark" | "light";
 }
 
-export default function Button({ buttonTxt, link }: buttonProps) {
-  return (
+export default function Button({ buttonTxt, link, theme }: buttonProps) {
+  return theme == "light" ? (
+    <a
+      href={link}
+      className="md:text-xl text-base border-2 font-bold text-white bg-none border-white px-5 py-2 rounded-full hover:bg-black hover:text-white hover:border-black duration-300"
+    >
+      {buttonTxt ? buttonTxt : "Know more"}
+    </a>
+  ) : (
     <a
       href={link}
       className="md:text-xl border-2 font-bold text-ardent bg-none border-ardent px-5 py-2 rounded-full hover:bg-ardent hover:text-black duration-300"
     >
-      {buttonTxt ? (
-        buttonTxt
-      ) : (
-        "Know more"
-      )}
+      {buttonTxt ? buttonTxt : "Know more"}
     </a>
   );
 }
