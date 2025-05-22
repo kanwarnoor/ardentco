224a26ccb634d0b02206e4a5d9f93ec98d2fef65
wellitsnoor <wellitsnoor@gmail.com>
2025-05-22 16:22:49 +0530

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Rethink_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
-const rethink = Rethink_Sans({ subsets: ["latin"], display: 'swap', adjustFontFallback: false });
+const rethink = Rethink_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  adjustFontFallback: false,
+});
 
 export const metadata: Metadata = {
   title: "Ardent Co",
@@ -17,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
-      <body className={rethink.className}>{children}</body>
+      <body className={rethink.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
