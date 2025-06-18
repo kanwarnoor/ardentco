ddf7d867f2442dcfd7920b6df384c1131d22ed95
wellitsnoor PC <wellitsnoor@gmail.com>
2025-06-18 22:30:05 +0530

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Rethink_Sans } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const rethink = Rethink_Sans({
   subsets: ["latin"],
@@ -21,7 +22,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
+      <head>
+        <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-GMWD07EPW2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GMWD07EPW2');
+          `}
+        </Script>
+      </head>
+
       <body className={rethink.className}>
         {children}
         <Analytics />
